097fb249e220547dc219b7076765eee6aad4ebb2
Willis Cao <wcao09@students.cs.ubc.ca>
2022-10-28 16:26:17 -0700

    Fixed checkstyle spacing.

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -24,7 +24,7 @@ public class JsonWriter {
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-       writer = new PrintWriter(new File(destination));
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
