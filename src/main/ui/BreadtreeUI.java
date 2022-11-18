9ad689b28f6d202f9b1c519d0eea57706e3f052e
Willis Cao <wcao09@students.cs.ubc.ca>
2022-11-18 12:34:32 -0800

    Implemented buttons to make a new notebook, delete a notebook, and save.

--- src/main/ui/BreadtreeUI.java
+++ src/main/ui/BreadtreeUI.java
@@ -13,6 +13,7 @@ import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,12 +32,13 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
     JPanel notebooksPanel;
     JPanel notebooksPanelLeft;
     JPanel notebooksPanelRight;
-    DefaultMutableTreeNode top;
+    DefaultMutableTreeNode rootNode;
     JTree notebookTree;
     JScrollPane treeView;
     JTable notebookTable;
     JScrollPane tableView;
     NotebookTableModel notebookTableModel;
+    JLabel dynamicLabel;
     JTextField entryField;
     JPanel buttonPanel;
     JButton newNotebookButton;
@@ -68,8 +70,8 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
         setResizable(false);
 
         // (color for debugging)
-        notebooksPanelLeft.setBackground(Color.BLUE);
-        notebooksPanelRight.setBackground(Color.RED);
+//        notebooksPanelLeft.setBackground(Color.BLUE);
+//        notebooksPanelRight.setBackground(Color.RED);
     }
 
     public void setupTabNotebooks() {
@@ -79,7 +81,7 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
         tabbedPane.addTab("Notebooks", notebooksPanel);
         notebooksPanelLeft = new JPanel();
         notebooksPanelLeft.setLayout(new BoxLayout(notebooksPanelLeft, BoxLayout.PAGE_AXIS));
-        notebooksPanelLeft.setPreferredSize(new Dimension(220, 600));
+        notebooksPanelLeft.setMinimumSize(new Dimension(220, 500));
         notebooksPanelRight = new JPanel();
         notebooksPanelRight.setLayout(new BoxLayout(notebooksPanelRight, BoxLayout.PAGE_AXIS));
 
@@ -88,19 +90,34 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
 
         setupTree();
         setupTable();
+        setupDynamicLabel();
         setupEntryField();
         setupButtons();
     }
 
     private void setupTree() {
         //tree showing the list of notebooks and the tags found in each one
-        top = new DefaultMutableTreeNode("My Notebooks");
-        updateNodes(top);
-        notebookTree = new JTree(top);
-        treeView = new JScrollPane(notebookTree);
+        treeView = new JScrollPane();
+        notebooksPanelLeft.add(treeView);
+        createNodes();
+    }
+
+    private void createNodes() {
+        rootNode = new DefaultMutableTreeNode("My Notebooks");
+        for (Notebook notebook:breadtree.getNotebooks()) {
+            DefaultMutableTreeNode notebookNode = new DefaultMutableTreeNode(notebook.getName());
+            rootNode.add(notebookNode);
+            for (String tag:notebook.getAllTags()) {
+                DefaultMutableTreeNode tagNode = new DefaultMutableTreeNode(tag);
+                notebookNode.add(tagNode);
+            }
+        }
+        notebooksPanelLeft.remove(treeView);
+        notebookTree = new JTree(rootNode);
         notebookTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         notebookTree.addTreeSelectionListener(this);
-        notebooksPanelLeft.add(treeView);
+        treeView = new JScrollPane(notebookTree);
+        notebooksPanelLeft.add(treeView, 0);
     }
 
     private void setupTable() {
@@ -113,6 +130,12 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
         notebookTableModel.fireTableDataChanged();
     }
 
+    private void setupDynamicLabel() {
+        dynamicLabel = new JLabel("");
+        dynamicLabel.setText("Test text...");
+        notebooksPanelRight.add(dynamicLabel);
+    }
+
     private void setupEntryField() {
         //text field for creating and editing entries
         entryField = new JTextField();
@@ -122,19 +145,40 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
     }
 
     private void setupButtons() {
+
         buttonPanel = new JPanel(new GridBagLayout());
-        //buttonPanel.setBorder(new TitledBorder("Notebook Options"));
-        GridBagConstraints c = new GridBagConstraints();
+        buttonPanel.setBorder(new TitledBorder("Notebook Options"));
+        buttonPanel.setMaximumSize(new Dimension(220, 200));
+
         newNotebookButton = new JButton("New");
+        newNotebookButton.addActionListener(this);
+        newNotebookButton.setActionCommand("newNotebookButton");
         deleteNotebookButton = new JButton("Delete");
+        deleteNotebookButton.addActionListener(this);
+        deleteNotebookButton.setActionCommand("deleteNotebookButton");
         saveButton = new JButton("Save");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        buttonPanel.add(newNotebookButton, c);
-        c.gridx = 1;
-        buttonPanel.add(deleteNotebookButton, c);
-        c.gridx = 2;
-        buttonPanel.add(saveButton, c);
+        saveButton.addActionListener(this);
+        saveButton.setActionCommand("saveButton");
+
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.weightx = 0.5;
+        c1.gridy = 0;
+        buttonPanel.add(newNotebookButton, c1);
+
+        GridBagConstraints c2 = new GridBagConstraints();
+        c2.fill = GridBagConstraints.HORIZONTAL;
+        c2.weightx = 0.5;
+        c2.gridy = 1;
+        buttonPanel.add(deleteNotebookButton, c2);
+
+        GridBagConstraints c3 = new GridBagConstraints();
+        c3.fill = GridBagConstraints.HORIZONTAL;
+        c3.weightx = 0.5;
+        c3.gridy = 2;
+
+        buttonPanel.add(saveButton, c3);
+
         notebooksPanelLeft.add(buttonPanel);
     }
 
@@ -150,14 +194,16 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
         }
     }
 
-    private void updateNodes(DefaultMutableTreeNode top) {
-        for (Notebook notebook:breadtree.getNotebooks()) {
-            DefaultMutableTreeNode notebookNode = new DefaultMutableTreeNode(notebook.getName());
-            top.add(notebookNode);
-            for (String tag:notebook.getAllTags()) {
-                DefaultMutableTreeNode tagNode = new DefaultMutableTreeNode(tag);
-                notebookNode.add(tagNode);
-            }
+    // Adapted from CPSC 210 JsonSerializationDemo
+    // EFFECTS: saves breadtree to file
+    private void saveBreadtree() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(breadtree);
+            jsonWriter.close();
+            System.out.println("Saved to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
         }
     }
 
@@ -170,31 +216,60 @@ public class BreadtreeUI extends JFrame implements ActionListener, TreeSelection
         }
 
         Object nodeInfo = node.getUserObject();
-        if (((DefaultMutableTreeNode) node.getParent()).isRoot()) {
+        if (node.isRoot()) {
+            return;
+        } else if (((DefaultMutableTreeNode) node.getParent()).isRoot()) {
             String notebook = (String) nodeInfo;
-            System.out.println(notebook);
-            notebookTableModel.updateDataFromNotebook(breadtree.getNotebookByName(notebook));
+            currentNotebook = breadtree.getNotebookByName(notebook);
+            notebookTableModel.updateDataFromNotebook(currentNotebook);
             notebookTableModel.fireTableDataChanged();
+            dynamicLabel.setText("Current notebook: " + notebook);
         } else if (node.isLeaf()) {
-            System.out.println("is leaf!");
             String tag = (String) nodeInfo;
             List<String> tagInList = new ArrayList<>();
             tagInList.add(tag);
             DefaultMutableTreeNode parentNotebookNode = (DefaultMutableTreeNode) node.getParent();
             Object parentNodeInfo = parentNotebookNode.getUserObject();
             Notebook parentNotebook = breadtree.getNotebookByName((String) parentNodeInfo);
+            currentNotebook = parentNotebook;
             Notebook filteredParentNotebook = new Notebook("", parentNotebook.getEntriesTagged(tagInList));
             notebookTableModel.updateDataFromNotebook(filteredParentNotebook);
             notebookTableModel.fireTableDataChanged();
+            dynamicLabel.setText("Viewing entries tagged \"" + tag + "\" in notebook \"" + parentNodeInfo
+                    + "\". Entries cannot be edited in this view.");
         }
     }
 
     public void actionPerformed(ActionEvent e) {
-//        if (e.getActionCommand().equals("myButton")) {
-//            label.setText(field.getText());
-//        }
         if (e.getActionCommand().equals("entryField")) {
             System.out.println(entryField.getText());
+        } else if (e.getActionCommand().equals("newNotebookButton")) {
+            String s = (String)JOptionPane.showInputDialog(
+                    this,
+                    "Enter a name for your new notebook:",
+                    "New Notebook",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    null);
+
+            if ((s != null) && (s.length() > 0)) {
+                breadtree.makeNotebook(s);
+                createNodes();
+            }
+        } else if (e.getActionCommand().equals("deleteNotebookButton")) {
+            int n = JOptionPane.showConfirmDialog(
+                    this,
+                    "Delete notebook \"" + currentNotebook.getName() + "\"?",
+                    "",
+                    JOptionPane.YES_NO_OPTION);
+            if (n == JOptionPane.YES_OPTION) {
+                breadtree.deleteNotebook(currentNotebook);
+                createNodes();
+            }
+        } else if (e.getActionCommand().equals("saveButton")) {
+            saveBreadtree();
+            dynamicLabel.setText("Save completed sucessfully.");
         }
     }
 
